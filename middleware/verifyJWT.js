eb0c0e1fcd0bc1f0ee66586483c7583afc3d66d6
anoopezhap <anoopezhap@gmail.com>
2024-02-21 16:40:29 -0500

    feb 21 work

--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -3,16 +3,21 @@ const jwt = require("jsonwebtoken");
 const verifyJWT = (req, res, next) => {
   const authHeader = req.headers.authorization || req.headers.Authorization;
 
+  console.log("req headers", req.headers);
+
   if (!authHeader?.startsWith("Bearer ")) {
     return res.status(401).json({ message: "Unauthorized" });
   }
 
   const token = authHeader.split(" ")[1];
+  //console.log("token", token);
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
     if (err) {
+      console.log(err);
       return res.status(403).json({ message: "Forbidden" });
     }
+    console.log("decoded", decoded);
     req.user = decoded.UserInfo.username;
     req.roles = decoded.UserInfo.roles;
     next();
